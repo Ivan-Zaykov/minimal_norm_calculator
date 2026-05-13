0447c14f945445caede6b0785ab5d527cc9b7031
ivan_zaykov <zaykov_ivan@list.ru>
2026-05-13 15:17:26 +0300

    New master: Hadamard simplex in R^31

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+int main() {
+    std::cout << "Hadamard simplex in R^31\n";
+    // Здесь будет: построить матрицу Адамара порядка 32,
+    // взять строки 2..32 как вершины симплекса,
+    // вычислить норму проектора и т.д.
+    return 0;
+}
